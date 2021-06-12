1ae10dc1a3196eebce0b650563807e29b5ced7a0
Vanessa <vanessamrios@gmail.com>
2021-06-11 23:45:19 -0300

    adiciona background color a li quando clicada

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ function createTask() {
         const inputValue = document.getElementById('texto-tarefa').value;
         const list = document.getElementById('lista-tarefas');
         let input = document.getElementById('texto-tarefa');
-        const task = document.createElement('li');
-        list.appendChild(task);
-        task.innerText = inputValue;
+        let tasks = document.createElement('li');
+        list.appendChild(tasks);
+        tasks.innerText = inputValue;
         input.value = '';
+        tasks.addEventListener('click', mudaCor)
+        function mudaCor () {
+            tasks=document.getElementsByTagName('li');
+            for (let task of tasks){
+                task.style.backgroundColor="rgb(128, 128, 128)"
+            }
         }
-    }
-createTask()
+}
+}
+createTask();       
